66bdefc99dd9cf78af1aea4cdbab97284554034f
IvanMPR <ivsanmas@gmail.com>
2024-07-24 21:44:46 +0200

    chore: Update file paths and imports for components

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,8 @@ import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
 import Filter from "../_components/Filter";
+import Reservation from "../_components/Reservation";
+import ReservationReminder from "../_components/ReservationReminder";
 
 export const metadata = { title: "Cabins" };
 
@@ -25,6 +27,7 @@ export default function Page({ searchParams }) {
       </div>
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
