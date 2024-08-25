b74c2acb9e898a2b8d319efde76ec116a04e97c6
IvanMPR <ivsanmas@gmail.com>
2024-08-25 19:21:10 +0200

    chore: Refactor DeleteReservation and ReservationCard components

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -37,6 +37,8 @@ export async function updateGuest(formData) {
 }
 
 export async function deleteReservation(bookingId) {
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
 
