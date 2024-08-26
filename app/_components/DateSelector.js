0d59ea9205cf4cd5375f25d4ff912146e0d888f4
IvanMPR <ivsanmas@gmail.com>
2024-08-26 19:13:32 +0200

    chore: Update file paths and imports for components

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { isWithinInterval } from "date-fns";
+import { differenceInDays, isWithinInterval, isPast } from "date-fns";
 
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
@@ -18,30 +18,30 @@ function isAlreadyBooked(range, datesArr) {
 
 function DateSelector({ settings, bookedDates, cabin }) {
   const { range, setRange, resetRange } = useReservation();
-  // CHANGE
-  const regularPrice = 23;
-  const discount = 23;
-  const numNights = 23;
-  const cabinPrice = 23;
-  // const range = { from: null, to: null };
-
+  const displayRange = isAlreadyBooked(range, bookedDates) ? {} : range;
+  const { regularPrice, discount } = cabin;
+  const numNights = differenceInDays(displayRange.to, displayRange.from);
+  const cabinPrice = (regularPrice - discount) * numNights;
   // SETTINGS
-  const { minBookingLength, maxBookingLength } = settings;
-
+  // const { minBookingLength, maxBookingLength } = settings;
+  console.log(bookedDates);
   return (
     <div className='flex flex-col justify-between'>
       <DayPicker
         className='pt-12 place-self-center'
         mode='range'
         onSelect={setRange}
-        selected={range}
-        min={minBookingLength + 1}
-        max={maxBookingLength}
+        selected={displayRange}
+        // min={minBookingLength + 1}
+        // max={maxBookingLength}
         fromMonth={new Date()}
         fromDate={new Date()}
         toYear={new Date().getFullYear() + 5}
         captionLayout='dropdown'
         numberOfMonths={2}
+        disabledDays={curDate =>
+          isPast(curDate) || bookedDates.some(date => isSameDay(date, curDate))
+        }
       />
 
       <div className='flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]'>
